6fa57a4f8b3081dcdce2c10b17abb8ba54adcfe9
Okaforudah <onyinyeokaforudah@gmail.com>
2020-03-25 14:58:14 -0700

    first javascript task

---
+++ js task1/main.js
@@ -0,0 +1,9 @@
+let name =("Precious Okaforudah");
+console.log("my name is " + name);
+
+const courses =["HTML","CSS","JavaScript"];
+console.log(courses);
+
+for(let i=0; i<=200; i++){
+    if(i % 2 !==0) console.log(i);
+}
